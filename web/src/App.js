f9447a06cc495a0c569c1f169d7ffe84ea024b5e
Anderson Vinicius Canelossi Valerio <a_vinicius18@cs.uni-kl.de>
2020-01-16 09:20:04 -0300

    web front-end

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import api from './services/api'
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
+
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
 
 function App() {
+  const [devs, setDevs] =  useState([]);
+
+  // load devs from mongoDB
+  useEffect(() => {
+    async function loadDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []); // Executed just 1x
+
+  async function addDev(data){
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm handleAddDev={addDev}/>
+      </aside>
+
+      <main>
+        <ul>  
+          {devs.map(dev =>(
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
+        </ul>
+      </main>
+
     </div>
   );
 }
